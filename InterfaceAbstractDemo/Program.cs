ce0c992f46d5c8a09194d01ea54c7772c10b7445
selimmemduhoglu <mselimmemduhoglu@gmail.com>
2022-11-29 00:10:35 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            { 
+                DateOfBirth = new DateTime(1999,7,6), 
+                FirstName="SAMET"  , 
+                LastName="AKCA", 
+                NationalityId="55555555"  
+            });
+
+            Console.ReadLine();
+        }
+
+       
+
+    }
+}
